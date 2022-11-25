a874813679ff4cce135d0e29d1d7d70011934fbd
pascal <pherms@outlook.com>
2022-11-25 15:54:54 +0100

    veranderingen in scripts

--- base-install.sh
+++ base-install.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
+# update sources-list
+isContrib=$(grep "main contrib non-free" /etc/apt/sources.list)
+if [[ -z "$isContrib" ]]; then
+  echo "non-free repos not enabled; enabling..."
+  echo "deb http://deb.debian.org/debian bullseye main contrib non-free" >> /etc/apt/sources.list
+  echo "deb http://security.debian.org/debian-security bullseye-security main contrib non-free" >> /etc/apt/sources.list
+  echo "deb http://deb.debian.org/debian bullseye-updates main contrib non-free" >> /etc/apt/sources.list
+fi
+
 apt update -y
-apt install git sudo screenfetch linux-firmware -y
+apt install -y git sudo screenfetch intel-microcode initramfs-tools firmware-linux
 
 # add regular user
-useradd -G sudo admin
-passwd admin
+# useradd -G sudo admin
+# passwd admin
 
 # configure screenfetch
 echo "if [ -f /usr/bin/screenfetch ]; then" >> /etc/profile
