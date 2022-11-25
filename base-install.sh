1f33835f6a59a09fdcad56a59d9dfd9c85fe18e7
pascal <pascal@brisbane.merel107.local>
2022-11-25 13:14:58 +0100

    rename base.sh -> base-install.sh

---
+++ base-install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+apt update -y
+apt install git sudo screenfetch linux-firmware -y
+
+# add regular user
+useradd -G sudo admin
+passwd admin
+
+# configure screenfetch
+echo "if [ -f /usr/bin/screenfetch ]; then" >> /etc/profile
+echo "    screenfetch;" >> /etc/profile
+echo "fi" >> /etc/profile
+
