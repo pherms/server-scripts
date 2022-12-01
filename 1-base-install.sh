ed2e546617c0fc3b078eb22f9b4b034543a512f0
pascal <pherms@outlook.com>
2022-12-01 20:09:40 +0100

    updated contrib non-free in sources.list

--- 1-base-install.sh
+++ 1-base-install.sh
@@ -22,9 +22,7 @@ fi
 isContrib=$(grep "main contrib non-free" /etc/apt/sources.list)
 if [[ -z "$isContrib" ]]; then
   echo "non-free repos not enabled; enabling..."
-  echo "deb http://deb.debian.org/debian bullseye main contrib non-free" >> /etc/apt/sources.list
-  echo "deb http://security.debian.org/debian-security bullseye-security main contrib non-free" >> /etc/apt/sources.list
-  echo "deb http://deb.debian.org/debian bullseye-updates main contrib non-free" >> /etc/apt/sources.list
+  sed -i 's/main/main contrib non-free/' /etc/apt/sources.list
 fi
 
 apt update -y
