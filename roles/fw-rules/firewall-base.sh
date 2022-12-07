447fad1c935a733967bf0b2b07159b3433eb545c
Pascal Herms <pherms@outlook.com>
2022-12-07 20:10:46 +0100

    diverse wijzigingen

--- roles/fw-rules/firewall-base.sh
+++ roles/fw-rules/firewall-base.sh
@@ -2,11 +2,21 @@
 flush ruleset                                                                    
 
 table inet firewall {
-    add set inet firewall LANv4 { type ipv4_addr\; flags interval\; }
-    add set inet firewall LANv6 { type ipv6_addr\; flags interval\; }
-    add element inet firewall LANv4 { 192.168.2.0/24 }
-    add element inet firewall LANv6 { 2a02:a462:72b3:1::/64, fe80::5604:a6ff:fe48:9010/64 }
-                                                                                 
+    # add set inet firewall LANv4 { type ipv4_addr\; flags interval\; }
+    # add set inet firewall LANv6 { type ipv6_addr\; flags interval\; }
+    # add element inet firewall LANv4 { 192.168.2.0/24 }
+    # add element inet firewall LANv6 { 2a02:a462:72b3:1::/64, fe80::5604:a6ff:fe48:9010/64 }
+
+    set LANv4 {
+        type ipv4_addr
+        flags interval
+        elements = { 192.168.2.0/24 }
+    }
+    set LANv6 {
+        type ipv6_addr
+        flags interval
+        elements = { 2a02:a462:72b3:1::/64, fe80::5604:a6ff:fe48:9010/64 }
+    }
     chain inbound_ipv4 {
         # accepting ping (icmp-echo-request) for diagnostic purposes.
         # However, it also lets probes discover this host is alive.
