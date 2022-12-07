06a47f889680a6b45aac00ed4094d074fc4a65ff
Pascal Herms <pherms@outlook.com>
2022-12-07 19:40:53 +0100

    diverse wijzigingen

--- roles/fw-rules/firewall-infra.sh
+++ roles/fw-rules/firewall-infra.sh
@@ -1,7 +1,7 @@
 #!/usr/sbin/nft -f
-
+#add rule inet filter input ip saddr @LANv4 tcp dport 3306 accept
 # Web
-add rule inet firewall input ip @LANv4 tcp dport { 123 } accept
-add rule inet firewall input ip @LANv4 udp dport { 53, 67, 68, 123, 161, 953 } accept
-add rule inet firewall input ip6 @LANv6 tcp dport { 123 } accept
-add rule inet firewall input ip6 @LANv6 udp dport { 53, 67, 68, 123, 161, 953 } accept
+add rule inet filter inbound ip saddr @LANv4 tcp dport { 123 } accept
+add rule inet filter inbound ip saddr @LANv4 udp dport { 53, 67, 68, 123, 161, 953 } accept
+add rule inet filter inbound ip6 saddr @LANv6 tcp dport { 123 } accept
+add rule inet filter inbound ip6 saddr @LANv6 udp dport { 53, 67, 68, 123, 161, 953 } accept
