e700c8cccbf50fe65016e22b917b697be79afac6
Pascal Herms <pherms@outlook.com>
2022-12-14 22:54:03 +0100

    troubleshooting

--- roles/firewall.sh
+++ roles/firewall.sh
@@ -11,6 +11,12 @@ systemctl start nftables.service
 echo "Geselecteerde rol: $role"
 # switch statement rol
 case $role in
+    web)
+      echo "Installeren basis firewall regel set"
+      ./roles/fw-rules/firewall-base.sh
+      echo "Installeren $role specifieke regel set"
+      ./roles/fw-rules/firewall-web.sh
+      ;;
     db)
       echo "Installeren basis firewall regel set"
       ./roles/fw-rules/firewall-base.sh
@@ -28,12 +34,6 @@ case $role in
       ./roles/fw-rules/firewall-base.sh
       echo "Installeren $role specifieke regel set"
       ./roles/fw-rules/firewall-proxy.sh
-      ;;
-    web)
-      echo "Installeren basis firewall regel set"
-      ./roles/fw-rules/firewall-base.sh
-      echo "Installeren $role specifieke regel set"
-      ./roles/fw-rules/firewall-web.sh
       ;;
     docker)
       echo "Installeren basis firewall regel set"
