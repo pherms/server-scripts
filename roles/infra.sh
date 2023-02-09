055d66e160247c9775eef07e86539465ff6585eb
Pascal Herms <pherms@outlook.com>
2023-02-09 19:11:08 +0100

    dhcp service units als files geconfigureerd en infr.sh script aangepast

--- roles/infra.sh
+++ roles/infra.sh
@@ -17,49 +17,36 @@ apt update
 echo "Installeren van elastic-agent"
 apt install -y metricbeat filebeat elastic-agent
 
+echo "Setup DHCP service Units"
 if [[ ! -f /etc/systemd/system/dhcp-server4.service ]]; then
-    echo "Setup DHCP service Units"
     echo "DHCP IPv4 Unit"
-    echo "[Unit]" >> /etc/systemd/system/dhcp-server4.service
-    echo "Description=ISC DHCP Server for IPv4 (dhcpd.conf)" >> /etc/systemd/system/dhcp-server4.service
-    echo "Wants=network-online.target" >> /etc/systemd/system/dhcp-server4.service
-    echo "After=network-online.target" >> /etc/systemd/system/dhcp-server4.service
-    echo -en '\n' >> /etc/systemd/system/dhcp-server4.service
-    echo "[Install]" >> /etc/systemd/system/dhcp-server4.service
-    echo "WantedBy=multi-user.target" >> /etc/systemd/system/dhcp-server4.service
-    echo -en '\n' >> /etc/systemd/system/dhcp-server4.service
-    echo "[Service]" >> /etc/systemd/system/dhcp-server4.service
-    echo "KillMode=mixed" >> /etc/systemd/system/dhcp-server4.service
-    echo "Type=forking" >> /etc/systemd/system/dhcp-server4.service
-    echo "PIDFile=/var/run/dhcpd.pid" >> /etc/systemd/system/dhcp-server4.service
-    echo "ExecStartPre=/usr/bin/touch /var/lib/dhcp/dhcpd.leases" >> /etc/systemd/system/dhcp-server4.service
-    echo "ExecStart=/usr/sbin/dhcpd -4 -q -cf /etc/dhcp/dhcpd.conf" >> /etc/systemd/system/dhcp-server4.service
+    cp ./roles/files/system/dhcp-server4.service /etc/systemd/system/
+    systemctl daemon-reload
     
-    echo "Enable en start DHCP server IPv4"
-    systemctl enable dhcp-server4.service
-    systemctl start dhcp-server4.service
+    echo "DHCP server voor IPv4 starten"
+    dhcp4status=$(systemctl show dhcp-server4 --property=SubState | awk 'BEGIN{FS="="} {print $2}')
+    if [[ "$dhcp4status" != "running" ]]; then
+        echo "Enable en start DHCP server IPv4"
+        systemctl enable dhcp-server4.service
+        systemctl start dhcp-server4.service
+    else
+        echo "DHCP server voor IPv4 is al gestart"
+    fi
 fi
 
 if [[ ! -f /etc/systemd/system/dhcp-server6.service ]]; then
     echo "DHCP IPv6 Unit"
-    echo "[Unit]" >> /etc/systemd/system/dhcp-server6.service
-    echo "Description=ISC DHCP Server for IPv6 (dhcpd6.conf)" >> /etc/systemd/system/dhcp-server6.service
-    echo "Wants=network-online.target" >> /etc/systemd/system/dhcp-server6.service
-    echo "After=network-online.target" >> /etc/systemd/system/dhcp-server6.service
-    echo -en '\n' >> /etc/systemd/system/dhcp-server6.service
-    echo "[Install]" >> /etc/systemd/system/dhcp-server6.service
-    echo "WantedBy=multi-user.target" >> /etc/systemd/system/dhcp-server6.service
-    echo -en '\n' >> /etc/systemd/system/dhcp-server6.service
-    echo "[Service]" >> /etc/systemd/system/dhcp-server6.service
-    echo "KillMode=mixed" >> /etc/systemd/system/dhcp-server6.service
-    echo "Type=forking" >> /etc/systemd/system/dhcp-server6.service
-    echo "PIDFile=/var/run/dhcpd6.pid" >> /etc/systemd/system/dhcp-server6.service
-    echo "ExecStartPre=/usr/bin/touch /var/lib/dhcp/dhcpd6.leases" >> /etc/systemd/system/dhcp-server6.service
-    echo "ExecStart=/usr/sbin/dhcpd -6 -q -cf /etc/dhcp/dhcpd6.conf" >> /etc/systemd/system/dhcp-server6.service
-
-    echo "Enable en start DHCP server IPv6"
-    systemctl enable dhcp-server6.service
-    systemctl start dhcp-server6.service
+    cp ./roles/files/system/dhcp-server6.service /etc/systemd/system/
+    systemctl daemon-reload
+    
+    dhcp6status=$(systemctl show dhcp-server6 --property=SubState | awk 'BEGIN{FS="="} {print $2}')
+    if [[ "$dhcp6status" != "running" ]]; then
+        echo "Enable en start DHCP server IPv6"
+        systemctl enable dhcp-server6.service
+        systemctl start dhcp-server6.service
+    else
+        echo "DHCP server voor IPv6 is al gestart"
+    fi
 fi
 
 echo "Enable en start bind DNS server"
