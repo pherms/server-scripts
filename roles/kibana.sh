5dbe2685a082fbc73b159a4a760b21adc9ee3a9c
Pascal Herms <pherms@outlook.com>
2023-03-01 00:37:04 +0100

    IP adres opvragen

--- roles/kibana.sh
+++ roles/kibana.sh
@@ -6,10 +6,17 @@ apt install -y default-jre nginx gpg curl apt-transport-https wget
 echo "Installeren van elasticsearch, kibana, metricbeat, filebeat, logstash en elastic-agent"
 apt install -y elasticsearch kibana logstash 
 
+echo "Opvragen IP adres"
+ipAddress=$(ip route | awk '/192.168.2/ { print $9 }')
+
 echo "Updaten elasticsearch config file"
+# network.host: 192.168.0.1 in /etc/elasticsearch/elasticsearch.yml
 sed -i 's/#network.host/network.host/' /etc/elasticsearch/elasticsearch.yml
+sed -i 's/192.168.0.1/$ipAddress' /etc/elasticsearch/elasticsearch.yml
 sed -i 's/#http.port/http.port/' /etc/elasticsearch/elasticsearch.yml
 
+echo "Controleren of de elasticsearch service is gestart"
+isElasticRunning=$(systemctl status elasticsearch | grep '(running)')
 echo "Start en enable de elasticsearch service"
 systemctl start elasticsearch
 systemctl enable elasticsearch
