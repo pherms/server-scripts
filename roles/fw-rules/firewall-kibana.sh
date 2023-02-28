9f937ce4676522032c5e0976eac89f509570167b
Pascal Herms <pherms@outlook.com>
2023-02-28 23:46:16 +0100

    kibana firewall config update

--- roles/fw-rules/firewall-kibana.sh
+++ roles/fw-rules/firewall-kibana.sh
@@ -1,4 +1,5 @@
 #!/usr/sbin/nft -f
 
 # Web
-add rule inet firewall inbound tcp dport { 9200, 5601 } accept
+add rule inet firewall inbound ip saddr @LANv4 tcp dport { 9200, 5601 } accept
+add rule inet firewall inbound ip6 saddr @LANv6 tcp dport { 9200, 5601 } accept
