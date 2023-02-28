8e3e6a83dde82b6361116f464e04025954afc1e0
Pascal Herms <pherms@outlook.com>
2023-02-28 22:59:00 +0100

    postfix config file toegevoegd en write config vervangen voor copy command

--- roles/mail.sh
+++ roles/mail.sh
@@ -10,18 +10,7 @@ echo "Installeren packages"
 apt install -y postfix openssl mailutils curl wget gpg apt-transport-https 
 
 echo "Writing postfix configuration"
-echo "smtpd_recipient_restrictions = reject_invalid_hostname, reject_unknown_recipient_domain, reject_unauth_destination, reject_rbl_client sbl.spamhaus.org, permit" >> /etc/postfix/main.cf
-echo "smtpd_helo_restrictions = reject_invalid_helo_hostname, reject_non_fqdn_helo_hostname, reject_unknown_helo_hostname" >> /etc/postfix/main.cf
-
-sed -i 's/^myhostname = .*/$hostname' /etc/postfix/main.cf
-sed -i 's/^mydestination = .*/$hostname.merel107.local, merel107.local, $hostname.merel107.local, localhost.merel107.local, localhost/' /etc/postfix/main.cf
-
-# sed -i 's/mydestination = .*/mydestination = $hostname.merel107.local, merel107.local, $hostname.merel107.local, localhost.merel107.local, localhost/' /etc/postfix/main.cf
-# sed -i 's/relayhost = .*/relayhost = [smtp.kpnmail.nl]:587/' /etc/postfix/main.cf
-# sed -i 's/smtp_sasl_auth_enable = .*/smtp_sasl_auth_enable = yes/' /etc/postfix/main.cf
-# sed -i 's/smtp_sasl_password_maps = .*/smtp_sasl_password_maps = hash:/etc/postfix/sasl_passwd/' /etc/postfix/main.cf
-# sed -i 's/smtp_use_tls = .*/smtp_use_tls = yes/' /etc/postfix/main.cf
-# sed -i 's/smtp_tls_cert_file = .*/smtp_tls_cert_file = /etc/pki/tls/certs/public.cert/' /etc/postfix/main.cf
+yes | cp ./roles/files/postfix/main.cf /etc/postfix/
 
 echo "Postfix is geconfigureerd"
 echo "Start en enable postfix service"
