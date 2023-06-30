4c9a6d05d98d038eafc5c610a9d3eb54f72c459f
Pascal Herms <pherms@outlook.com>
2023-06-30 16:12:20 +0200

    Feature/auto updater (#39)
    * Initieel werk aan autouopdater
    
    * check of er changes zijn
    
    * echo en timer scripts toegevoegd
    
    * testen
    
    * testen
    
    * testen
    
    * testen
    
    * testen
    
    * testen
    
    * testen
    
    * testen
    
    * testen
    
    * testen
    
    * testen en cleanup

---
+++ autoupdater.sh
@@ -0,0 +1,15 @@
+#/bin/bash
+
+cd /scripts/server-scripts
+
+# git checkout main
+git fetch
+changes=$(git diff origin/feature/auto-updater)
+
+if [[ -n "$changes" ]]; then
+    echo "Changes in remote gdetecteerd. Nu downloaden"
+    git pull
+    git branch | grep -v "main" | xargs git branch -D
+else
+    echo "Er zijn geen veranderingen"
+fi
