659eb05ea1aa2447e0b240e9b73996d8a2e8544e
Pascal Herms <pherms@outlook.com>
2023-06-30 15:50:03 +0200

    check of er changes zijn

--- autoupdater.sh
+++ autoupdater.sh
@@ -1,6 +1,12 @@
 #/bin/bash
 
 cd /scripts/server-scripts
-git pull
+git fetch
+changes=$(git diff origin/main)
+
+if [[ -n "$changes"]]; then
+    echo "Changes in remote gdetecteerd. Nu downloaden"
+    git pull
+
 git branch | grep -v "main" | xargs git branch -D
 # nu een verandering
