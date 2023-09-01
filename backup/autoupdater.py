7fcfabb09668bad0d3c1bf7fb10ced5362532e18
Pascal Herms <pherms@outlook.com>
2023-09-01 12:22:44 +0200

    troubleshooting response

--- backup/autoupdater.py
+++ backup/autoupdater.py
@@ -79,7 +79,7 @@ def main():
                 if requestZip.ok:
                     zipFile = zipfile.ZipFile(io.BytesIO(requestZip.content))
                     zipcontent = zipFile.namelist()
-                    zipinfo = zipFile.getinfo()
+                    zipinfo = zipFile.getinfo(zipFile)
                     if debug:
                         print("[DEBUG] zipcontent: {}".format(zipcontent))
                         print("[DEBUG] zipinfo: {}".format(zipinfo))
