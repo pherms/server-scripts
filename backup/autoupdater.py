ded9ec9df8a350956dea7b508e9db6cb47d7b345
Pascal Herms <pherms@outlook.com>
2023-09-01 11:13:35 +0200

    troubleshooting response

--- backup/autoupdater.py
+++ backup/autoupdater.py
@@ -23,14 +23,15 @@ def main():
 
         try:
             response = requests.get(apiurl)
+            print(response)
 
             if response.status_code == 200:
                 if debug:
                     print("[DEBUG] API request succesvol uitgevoerd: {}".format(response.status_code))
 
                 logfile.write("{} API request naar {} succesvol uitgevoerd. Status code: {}\n".format(datetime.today(),apiurl,response.status_code))    
-                responseObject = json.load(response)
-                print(response)
+                responseObject = json.load(response.json())
+
                 latestVersion = responseObject.tag_name
                 zipUrl = responseObject.zipball_url
         except Exception as error:
