631334e3d63e2eba79e59a5c30e90b9c37e44f32
Pascal Herms <pherms@outlook.com>
2023-09-07 13:57:29 +0200

    daemons

--- backup/modules/daemons.py
+++ backup/modules/daemons.py
@@ -1,4 +1,6 @@
 import os
+import subprocess
+from datetime import datetime
 
 def stopDaemon(daemon,logfile):
     pass
@@ -14,3 +16,25 @@ def installDaemon(daemon,logfile):
 
 def reloadDaemon(daemon,logfile):
     pass
+
+def checkIfDaemonIsRunnning(daemon,logfile,debug):
+    ps = subprocess.Popen(('ps', '-ef'), stdout=subprocess.PIPE)
+    grep = subprocess.Popen(('grep', '-v', 'grep'), stdin=ps.stdout, stdout=subprocess.PIPE)
+
+    ps.stdout.close()  # Allow ps to receive a SIGPIPE if grep exits.
+
+    grep_daemon = subprocess.Popen(('grep', 'sshd'), stdin=grep.stdout)
+
+    grep.stdout.close() # Allow grep to receive a SIGPIPE if grep_daemon exits
+
+    output = grep_daemon.communicate()[0]
+    if output.lower() == "none":
+        if debug:
+            print("[DEBUG] Daemon {} is niet actief".format(daemon))
+        logfile.write("{} Daemon {} is niet actief\n".format(datetime.today(),daemon))
+        return False
+    else:
+        if debug:
+            print("[DEBUG] daemon {} is actief. Return True".format(daemon))
+        logfile.write("{} Daemon {} is actief\n".format(datetime.today(),daemon))
+        return True
