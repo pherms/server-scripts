c4689eb5195bf42de357af7c0ee839abd9ef9b5e
Pascal Herms <pherms@outlook.com>
2023-09-21 16:09:55 +0200

    fixed autoupdate dingen

--- backup/modules/logging.py
+++ backup/modules/logging.py
@@ -23,9 +23,6 @@ def openLogFile(logpath,logfiletype,debug):
         logfilename = "updatelog.log"
 
     try:
-        # if Path(logpath + logfilename).exists():
-        #     logfile = open(logpath + logfilename,"a")
-        # else:
         logfile = open(logpath + logfilename,"w")
 
         if debug:
