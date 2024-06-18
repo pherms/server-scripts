ae5662bcd399a7c21c6414646baeeff4070f7786
Pascal Herms <pherms@outlook.com>
2024-06-18 16:19:07 +0200

    add install module en script

---
+++ backup/installconfig.py
@@ -0,0 +1,172 @@
+import os
+import json
+import requests
+from json import JSONDecodeError
+import modules as mods
+from datetime import datetime
+from pathlib import Path
+
+def main():
+    config = mods.readConfig()
+    logfilepath = config["logfilepath"]
+    debug = bool(config["debug"])
+    scriptfolder = config["scriptspath"]
+    apiurl = config["apiurl"]
+    timers = config["timerUnits"]
+    serverApiDir = config["serverApiDir"]
+    clientConfigDir = config["clientConfigDir"]
+
+    logfile = mods.openLogFile(logfilepath,"update",debug)
+    hostname = mods.getHostname(logfile)
+
+# Downloading zip
+    try:
+        response = requests.get(apiurl)
+
+        if response.status_code == 200:
+            if debug:
+                print("[DEBUG] API request succesvol uitgevoerd: {}".format(response.status_code))
+
+            logfile.write("{} API request naar {} succesvol uitgevoerd. Status code: {}\n".format(datetime.today(),apiurl,response.status_code))
+            responseDictionary = response.json()
+            
+            if debug:
+                prettyJson = json.dumps(responseDictionary, indent=4)
+                print(prettyJson)
+
+            latestVersion = responseDictionary.get('tag_name')
+            zipUrl = responseDictionary.get('zipball_url')
+            if debug:
+                print("[DEBUG] latest version: {}\n[DEBUG] zipUrl: {}".format(latestVersion,zipUrl))
+        else:
+            print("Er is iets fout gegaan bij het uitvoeren van de API request naar Github. Het script wordt afgebroken")
+            logfile.write("{} Er is iets fout gegaan bij het uitvoeren van de API request naar Github. Het proces wordt gestopt\n".format(datetime.today()))
+            exit()
+
+    except JSONDecodeError as jsonerror:
+        if debug:
+            print("[DEBUG] Er is iets fout gegaan tijdens het decoden van de JSON.\n[DEBUG] De error is: {}".format(jsonerror))
+
+        logfile.write("{} Er is iets fout gegaan tijdens het decoden van de JSON. De error is: {}\n".format(datetime.today(),jsonerror))
+        exit()
+
+    except Exception as error:
+        logfile.write("{} Er is iets fout gegaan tijdens het uitvoeren van de API request. Status code: {}\n".format(datetime.today(),response.status_code))
+        logfile.write("{} De error is: {}\n".format(datetime.today(),error))
+        if debug:
+            print("[DEBUG] fout bij uitvoeren API request: {}. Zie logfile".format(response.status_code))
+        exit()
+
+# Installeer daemons
+    try:
+        # Installeer daemons. Eerst daemons, dan timers
+        # Service moet worden geïnstalleerd, maar wordt gestart door een timer
+        
+        serviceToInstall = "config-server-api.service"
+        status = mods.checkIfDaemonIsNotInstalled(serviceToInstall,logfile,debug)
+
+        if status:
+            installedService = mods.installDaemon(serviceToInstall,logfile,debug,scriptfolder,status)
+        else:
+            compareResult = mods.compareDaemonFiles(serviceToInstall,logfile,scriptfolder,debug) # False betekent files zijn niet gelijk, nieuwe kopieren
+            installedService = mods.installDaemon(serviceToInstall,logfile,debug,scriptfolder,compareResult)
+
+        if installedService == 'installed':
+            mods.enableDaemon(serviceToInstall,logfile,debug)
+        if installedService == 'updated':
+            mods.reloadDaemon(logfile,debug)
+        if installedService == 'error':
+            logfile.write("{} Er is een fout opgetreden tijden het installeren van service: {}\n".format(datetime.today(),serviceToInstall))
+            if debug:
+                print("[DEBUG] Er is een fout opgetreden bij het installeren van service: {}".format(serviceToInstall))
+
+    except Exception as error:
+        logfile.write("{} Er is iets fout gegaan tijdens het installeren van de service {}\n".format(datetime.today(),servicesToInstall))
+        logfile.write("{} De foutmelding is: {}\n".format(datetime.today(),error))
+        if debug:
+            print("[DEBUG] Er is iets fout gegaan tijdens het installeren van de service {}. De error is: {}".format(servicesToInstall,error))
+        exit()
+
+# Installing client en server
+    try:
+        requestZip = requests.get(zipUrl)
+        print(requestZip)
+        if debug:
+            print("[DEBUG] zipfile is gedownload. statuscode: {}".format(requestZip.status_code))
+            
+        if requestZip.ok:
+            logfile.write("{} De zipfile {} is succesvol gedownload\n".format(datetime.today(),zipUrl))
+            zipFile = zipfile.ZipFile(io.BytesIO(requestZip.content))
+            folderInZip = zipFile.namelist()[0]
+            tempFolder = "/tmp/" + folderInZip[:-1]
+
+            os.chdir('/tmp')
+            zipFile.extractall()
+            logfile.write("{} De zipfile is uitgepakt naar directory {}\n".format(datetime.today(),tempFolder))
+            
+            # delete tests directory
+            if os.path.exists(os.path.join(tempFolder,"backup/tests/")):
+                # shutil.rmtree(os.path.join(tempFolder,"backup/tests/"))
+                mods.deleteDirectory(os.path.join(tempFolder,"backup/tests/"),logfile)
+        
+        if os.path.exists(tempFolder):
+            os.system("cp -r {}/* {}".format(tempFolder,scriptfolder))
+            # remove config dir
+            # shutil.rmtree(os.path.join(scriptfolder,"config"))
+            mods.deleteDirectory(os.path.join(scriptfolder,"config"),logfile)
+
+            # api server
+            serverDir = Path(os.path.join(tempFolder,"config/server/"))
+            # index = serverDir.parts.index('src')
+            # workingDir = os.path.join(serverDir,"src")
+
+            os.chdir(serverDir)
+            # shutil.rmtree("dist")
+            mods.deleteDirectory(os.path.join(serverDir,"dist"),logfile)
+
+            # recreate build folder en build app
+            os.mkdir("dist")
+            # os.system("npm run build")
+            mods.compileSource("server",logfile)
+
+            mods.installFiles("server",tempFolder,logfile)
+            mods.restartDaemon("config-server-api",logfile,debug)
+            # os.system("cp -r {}/ {}".format(os.path.join(serverDir,"dist"),serverApiDir))
+
+            # for file in workingDir.glob("**/*.js"):
+            #     source = file.absolute()
+            #     destination = Path(serverApiDir).joinpath(*source.parts[index+1:])
+
+            #     os.system("cp {} {}".format(source,destination))
+            # End api server
+
+            # config client
+            clientDir = Path(os.path.join(tempFolder,"config/client/"))
+            os.chdir(clientDir)
+            # shutil.rmtree("dist")
+            mods.deleteDirectory(os.path.join(clientDir,"dist"),logfile)
+
+            # recreate build folder en build app
+            os.mkdir("dist")
+            # os.system("npm run build")
+            mods.compileSource("client",logfile)
+
+            # os.system("cp -r {}/ {}".format(os.path.join(clientDir,"dist"),clientConfigDir))
+            mods.installFiles("client",tempFolder,logfile)
+            mods.restartDaemon("apache2",logfile,debug)
+            # end config client
+
+            logfile.write("{} De bestanden zijn gekopieerd naar directory: {}\n".format(datetime.today(),scriptfolder))
+            if debug:
+                print("[DEBUG] Bestanden zijn gekopieerd naar de scriptsfolder")
+    
+    except Exception as error:
+        logfile.write("{} Er is iets fout gegaan tijdens het downloaden van de zipfile\n".format(datetime.today()))
+        logfile.write("{} De foutmelding is: {}\n".format(datetime.today(),error))
+        if debug:
+            print("[DEBUG] Er is iets fout gegaan tijdens het downloaden van de zip. De error is: {}".format(error))
+        exit()
+
+
+if __name__ == '__main__':
+    main()
---
+++ backup/modules/install.py
@@ -0,0 +1,61 @@
+import modules as mods
+import os
+from pathlib import Path
+from datetime import datetime
+
+def determineConfigFolder(type,tempFolder):
+    """
+    Het type config bepalen. De return waarde is een folder
+
+    :param str type: Het type wat moet worden behandeld. Mogelijke waarden zijn: client en server
+    :param str tempFolder: de tempFolder waar de bestanden naar zijn uitgepakt
+    :return: sourceDir,destinationDir, De sourceDir en destinationDir path objecten
+    :rtype: tuple
+    """
+    config = mods.readConfig()
+
+    match type:
+        case "client":
+            sourceDir = Path(os.path.join(tempFolder,"config/client/"))
+            destinationDir = config["clientConfigDir"]
+        case "server":
+            sourceDir = Path(os.path.join(tempFolder,"config/server/"))
+            destinationDir = config["serverApiDir"]
+    
+    return (sourceDir,destinationDir)
+
+def installFiles(type,tempFolder,logfile):
+    """
+    Het kopieren van de nieuwe bestanden
+
+    :param str type: Het type wat moet worden behandeld. Mogelijke waarden zijn: client en server
+    :param str tempFolder: de tempFolder waar de bestanden naar zijn uitgepakt
+    :return: sourceDir,destinationDir, De sourceDir en destinationDir path objecten
+    :rtype: tuple
+    """
+    logfile.write("{} Gevonden type installatie: {}\n".format(datetime.today(),type))
+    logfile.write("{} Bepalen van de bron en doel directories\n".format(datetime.today()))
+
+    (sourceDir,destinationDir) = mods.determineConfigFolder(type,tempFolder)
+    index = sourceDir.parts.index('src')
+    workingDir = os.path.join(sourceDir,"src")
+
+    try:
+        logfile.write("{} Kopieren van de gecompilede sources\n".format(datetime.today()))
+        os.system("cp -r {}/ {}".format(os.path.join(sourceDir,"dist"),destinationDir))
+
+        if (type == "server"):
+            logfile.write("{} Kopieren van additionele javascript files tbv server\n".format(datetime.today()))
+            for file in workingDir.glob("**/*.js"):
+                source = file.absolute()
+                destination = Path(destinationDir).joinpath(*source.parts[index+1:])
+
+                os.system("cp {} {}".format(source,destination))
+    except Exception as error:
+        print("Er is een fout opgetreden. {}".format(error))
+        logfile.write("{} Er is een fout opgetreden. De error is:\n{}".format(datetime.today(),error))
+        exit()
+
+def compileSource(type,logfile):
+    logfile.write("{} Compileren van {} source\n".format(datetime.today(),type))
+    os.system("npm run build")
