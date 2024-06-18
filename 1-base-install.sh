48a52dcf32432f9ae1c8806853407012259b188e
Pascal Herms <pherms@outlook.com>
2024-06-18 16:44:53 +0200

    error in install script

--- 1-base-install.sh
+++ 1-base-install.sh
@@ -90,8 +90,8 @@ yes | cp ./backup/config.json /etc/server-scripts/backup-config.json
 yes | cp ./backup/sources /etc/server-scripts/
 
 # Configure DNS
-resolvectl dns ens18 $dnsserver
-resolvectl domain ens18 $domainname
+# resolvectl dns ens18 $dnsserver
+# resolvectl domain ens18 $domainname
 
 # reload systemctl daemon en enable en start services
 systemctl daemon-reload
