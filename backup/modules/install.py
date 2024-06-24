d81bfa5e41260e00de4f15e2724885f99b35e6c2
Pascal Herms <pherms@outlook.com>
2024-06-24 19:09:32 +0200

    Update install.py destination convert naar path

--- backup/modules/install.py
+++ backup/modules/install.py
@@ -54,7 +54,7 @@ def installFiles(type,tempFolder,logfile):
     try:
         logfile.write("{} Kopieren van de gecompilede sources\n".format(datetime.today()))
         print("Kopieren van additionele files. functie: installFiles (2e deel)")
-        os.system("cp -a {}/. {}".format(os.path.join(sourceDir,"dist"),destinationDir))
+        os.system("cp -a {}/. {}".format(os.path.join(sourceDir,"dist"),Path(destinationDir)))
 
         if type == "server":
             logfile.write("{} Kopieren van additionele javascript files tbv server\n".format(datetime.today()))
@@ -68,10 +68,10 @@ def installFiles(type,tempFolder,logfile):
             # yes | cp /scripts/server-scripts/config/server/package.json $serverapidir
             # yes | cp -a /scripts/server-scripts/config/server/prisma/ $serverapidir
             logfile.write("{} Kopieren van package.json\n".format(datetime.today()))
-            os.system("cp {} {}".format(os.path.join(sourceDir,"package.json"),destinationDir))
+            os.system("cp {} {}".format(os.path.join(sourceDir,"package.json"),Path(destinationDir)))
 
             logfile.write("{} Kopieren van prisma directory\n".format(datetime.today()))
-            os.system("cp -r {} {}".format(os.path.join(sourceDir,"prisma"),destinationDir))
+            os.system("cp -r {} {}".format(os.path.join(sourceDir,"prisma"),Path(destinationDir)))
 
     except Exception as error:
         print("Er is een fout opgetreden. {}".format(error))
